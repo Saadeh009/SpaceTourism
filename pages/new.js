964a7ecc0a1df17cb318d90e64689be1872d06ea
Saadeh <saadehelias9@gmail.com>
2022-05-07 12:52:47 +0300

    first steps

---
+++ pages/new.js
@@ -0,0 +1,10 @@
+import styles1 from '../styles/Home2.module.css'
+
+export default function New() {
+ return (
+  <>
+  <h1 className={styles1.testing}>hiii</h1>
+  <h1 className={styles1.earth}>EARTH</h1>
+  </>
+ )
+}
