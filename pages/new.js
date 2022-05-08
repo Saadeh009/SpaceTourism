a7b430a20a1eff307409f6eccfb00eba863308a2
Saadeh <saadehelias9@gmail.com>
2022-05-08 15:40:07 +0300

    finished simple css classes

--- pages/new.js
+++ pages/new.js
@@ -1,10 +1,13 @@
 import styles1 from '../styles/Home2.module.css'
 
+//css classes in root directory are working
 export default function New() {
  return (
   <>
+  
   <h1 className={styles1.testing}>hiii</h1>
   <h1 className={styles1.earth}>EARTH</h1>
+  <p className={styles1.subheading}>A subheading</p>
   </>
  )
 }
